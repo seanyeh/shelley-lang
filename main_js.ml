0349614dbcdbc15b0f5522aa24383161310c9947
Sean Yeh <sean.yeh@gmx.com>
2014-07-31 14:04:14 -0400

    minor js things

--- main_js.ml
+++ main_js.ml
@@ -1,5 +1,5 @@
 let compile_js_string source =
     Main.compile_string (Js.to_string source)
 in
-    Js.Unsafe.global##compile <- Js.wrap_callback compile_js_string
+    Js.Unsafe.global##shlycompile <- Js.wrap_callback compile_js_string
 
